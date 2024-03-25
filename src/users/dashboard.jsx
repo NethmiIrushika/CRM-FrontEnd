955672637f035e04cd59651eaa04ed5b3089f670
kumaljaye <157775455+kumaljaye@users.noreply.github.com>
2024-03-25 21:31:28 +0530

    dash

--- src/users/dashboard.jsx
+++ src/users/dashboard.jsx
@@ -115,6 +115,7 @@ const Dashboard = () => {
             <li>
               <button
                 onClick={() => navigate("ongingCr")}
+                style={{ display: userType !== "Developer" ? "none" : "" }}
                 className="flex items-center text-white rounded-lg hover:bg-white hover:bg-opacity-40 hover:ring-1 hover-ring-white "
               >
                 <div className="flex items-center w-40 h-10">

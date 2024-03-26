ddf5b281c52d33a03acd15206a8d8caae392a2c9
wniruu <nethmii.bg2023a@gmail.com>
2024-03-26 15:41:06 +0530

    priority change number remove

--- src/component/approve_or_rejectedCr.jsx
+++ src/component/approve_or_rejectedCr.jsx
@@ -196,7 +196,7 @@ function ApproveORreject() {
             {pr.rejectionReason == null && (
               <button
                 onClick={() => handleChangeStatusButtonClick(pr.prId)}
-                className="mt-2 bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+                className="mt-2 bg-green-500 hover:bg-green-700 text-white font-bold py-2 ml-2 px-4 rounded"
               >
                 Complete Task
               </button>

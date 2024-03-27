8f3a1a2ad0ffb003a87844df5e0789a030e44ae7
kumaljaye <157775455+kumaljaye@users.noreply.github.com>
2024-03-27 14:18:42 +0530

    date and time

--- src/component/approveprototype.jsx
+++ src/component/approveprototype.jsx
@@ -3,8 +3,7 @@ import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import api from "../api";
 import { getLoginInfo } from "../utils/LoginInfo";
-
-
+import { format } from "date-fns"; // Import format function from date-fns
 
 function Approveprototype() {
   const [crprototype, setCrprototype] = useState([]);
@@ -35,15 +34,18 @@ function Approveprototype() {
   };
 
   const filteredCrPrototypes = crprototype.filter((pr) => {
-    return pr && pr.cr && pr.cr.userId && pr.cr.userId.userId === getLoginInfo()?.sub &&
-    pr.popupstatus !== "Rejected" &&
-    pr.popupstatus !== "Approved" && 
-    pr.rejectionReason !== "" &&
-    pr.cr.status !== "Completed" &&
-    pr.cr.status !== "Develop without Prototype";
-
+    return (
+      pr &&
+      pr.cr &&
+      pr.cr.userId &&
+      pr.cr.userId.userId === getLoginInfo()?.sub &&
+      pr.popupstatus !== "Rejected" &&
+      pr.popupstatus !== "Approved" &&
+      pr.rejectionReason !== "" &&
+      pr.cr.status !== "Completed" &&
+      pr.cr.status !== "Develop without Prototype"
+    );
   });
-
 
   useEffect(() => {
     fetchCrprototype();
@@ -52,6 +54,10 @@ function Approveprototype() {
   const handleActionClick = (prId) => {
     console.log("cr Id:", prId);
     navigate(`/dashboard/showprotoDetails/${prId}`);
+  };
+
+  const formatDate = (date) => {
+    return format(new Date(date), 'dd/MM/yyyy HH:mm:ss'); // Format date using date-fns
   };
 
   return (
@@ -68,37 +74,46 @@ function Approveprototype() {
 
       <div className="grid grid-cols-1 gap-4">
         {filteredCrPrototypes.map((pr) => (
-          <div key={pr.prId} className="bg-white rounded-lg shadow-md p-6" >
-            <div className="grid grid-cols-2 gap-2 ">
-              <div className="col-span-2 ">
+          <div key={pr.prId} className="bg-white rounded-lg shadow-md p-6">
+            <div className="grid grid-cols-2 gap-2">
+              <div className="col-span-2">
                 <p className="text-lg font-bold text-right">
                   <span className="text-black-400">Status: </span>
                   <span
-                    className={`${pr.cr.status === 'Sent prototype' ? 'text-blue-500' : pr.cr.status === 'Pending' ? 'text-red-500' : ''
-                      }`}
+                    className={`${
+                      pr.cr.status === "Sent prototype"
+                        ? "text-blue-500"
+                        : pr.cr.status === "Pending"
+                        ? "text-red-500"
+                        : ""
+                    }`}
                   >
                     {pr.cr.status}
                   </span>
                 </p>
 
-                <p className="text-lg font-bold text-stone-950 text-left">Topic: {pr.topic}</p>
+                <p className="text-lg font-bold text-stone-950 text-left">
+                  Topic: {pr.topic}
+                </p>
               </div>
-              <div className="col-span-2 ">
-              {pr.cr.createdAt && (
-                  <p className=" text-center"> The Change Request with ID: <span className="font-semibold text-xl">{pr.crId}</span> you created at : <span className="font-semibold">{pr.cr.createdAt}</span> now has a Prototype. Plese make sure to View the prototype and give your feed backs</p> 
+              <div className="col-span-2">
+                {pr.cr.createdAt && (
+                  <p className="text-center">
+                    The Change Request with ID:{" "}
+                    <span className="font-semibold text-lg">{pr.crId}</span>{" "}
+                    you created at :{" "}
+                    <span className="font-semibold">
+                      {formatDate(pr.cr.createdAt)} {/* Format date */}
+                    </span>{" "}
+                    now has a Prototype. Plese make sure to View the prototype
+                    and give your feed backs
+                  </p>
                 )}
               </div>
-              {/* <div className="col-span-1">
-                {pr.cr && <p className="mb-2 text-left">name: {pr.cr.name}</p>}
-              </div> */}
             </div>
-            {/* <div className="col-span-1">
-              <p className=" mb-2 text-left">Description:</p>
-              <div className="bg-gray-200 p-4 mt-4 rounded-lg">{pr.description}</div>
-            </div> */}
             <button
               onClick={() => handleActionClick(pr.prId)}
-              className="mt-4  bg-yellow-400 hover:bg-yellow-500 text-black font-bold py-2 px-4 rounded"
+              className="mt-4 bg-yellow-400 hover:bg-yellow-500 text-black font-bold py-2 px-4 rounded"
             >
               View
             </button>

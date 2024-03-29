34ce87f9f069b0076816674ea84e7f7ac00d3c35
hashdila <dilannawa@gmail.com>
2024-03-29 11:14:22 +0530

    date

--- src/component/crProtoType.jsx
+++ src/component/crProtoType.jsx
@@ -1,17 +1,17 @@
-// CrProtoType.jsx
-
 import React, { useState, useEffect } from 'react';
 import { useNavigate, useParams, useLocation } from 'react-router-dom';
 import api from '../api';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import DatePicker from 'react-datepicker'; // Import react-datepicker
+import 'react-datepicker/dist/react-datepicker.css'; 
 
 const CrProtoType = () => {
   const { crId } = useParams(); // Extract crId from URL parameters
   const {state} = useLocation();
   const [formData, setFormData] = useState({
     topic: state?.topic ||'',
-    description: ''
+    description: '',deleverDate:'',
   });
   const [file, setFile] = useState(null);
   const navigate = useNavigate();
@@ -47,6 +47,7 @@ const CrProtoType = () => {
       formDataToSend.append('topic', formData.topic);
       formDataToSend.append('crId', crId);
       formDataToSend.append('file', file);
+      formDataToSend.append('deliveryDate', formData.deleverDate);
   
       const response = await api.post('/crprototype/', formDataToSend);
   
@@ -69,6 +70,8 @@ const CrProtoType = () => {
       toast.error('Error creating change request prototype. Please try again.');
     }
   };
+
+  const [deliveryDate, setDeliveryDate] = useState(null);
 
   return (
     <div className="max-w-lg mx-auto bg-white shadow-lg rounded-lg p-12">
@@ -114,6 +117,19 @@ const CrProtoType = () => {
               id="file"
               className="block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 focus:outline-none"
               onChange={handleFileUpload}
+            />
+          </div>
+          <div>
+            <label htmlFor="deliveryDate" className="block text-sm font-medium text-gray-700 mb-1">
+              Delivery Date:
+            </label>
+            <DatePicker
+              id="deliveryDate"
+              selected={deliveryDate}
+              onChange={date => setDeliveryDate(date)} // Update deliveryDate state when date is selected
+              className="block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500 focus:outline-none"
+              dateFormat="yyyy-MM-dd" // Specify the date format
+              placeholderText="Select delivery date" // Placeholder text for the date picker
             />
           </div>
 
